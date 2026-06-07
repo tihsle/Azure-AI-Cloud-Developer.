9d6f3fe3e4e637fcdb4986b019279c091092b888
kuljotSB <kuljot.bakshi302005@gmail.com>
2026-06-07 14:52:59 +0530

    added sentinel refresh lab

--- Azure-App-Configuration/ChatBackend/app.py
+++ Azure-App-Configuration/ChatBackend/app.py
@@ -2,44 +2,105 @@ import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS  # Import Flask-CORS
 from openai import AzureOpenAI
+from azure.appconfiguration.provider import load, SettingSelector, WatchKey
+from azure.identity import DefaultAzureCredential
+from featuremanagement import FeatureManager
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes and origins
 
-# Read configuration from environment variables
-AZURE_OPENAI_ENDPOINT = os.environ.get("azure-api-url")
-AZURE_OPENAI_API_KEY = os.environ.get("azure-api-key")
-AZURE_OPENAI_MODEL_NAME = os.environ.get("azure-model-name")
-AZURE_OPENAI_API_VERSION = os.environ.get("azure-api-version", "2024-12-01-preview")
+# creating the App Configuration Selector Object
+selects = [
+    SettingSelector(key_filter="AzureOpenAI:*", label_filter="\0"),
+    SettingSelector(key_filter="AzureOpenAI:*", label_filter="Production")
+]
 
-client = AzureOpenAI(
-    api_version=AZURE_OPENAI_API_VERSION,
-    azure_endpoint=AZURE_OPENAI_ENDPOINT,
-    api_key=AZURE_OPENAI_API_KEY,
+# Using the System Assigned Identity Credential
+credential = DefaultAzureCredential()
+
+# Creating the App Configuration Loader Client Object
+config = load(
+    endpoint = os.getenv("AZURE_APPCONFIG_ENDPOINT"),
+    credential=credential,
+    selects = selects,
+    feature_flag_enabled = True,
+    feature_flag_refresh_enabled = True,
+    refresh_on = [WatchKey("Sentinel")],
+    keyvault_credential=credential,
+    refresh_interval = 30
 )
+
+# Creating the Feature Manager Object
+feature_manager = FeatureManager(config)
 
 @app.route("/chat", methods=["POST"])
 def chat():
-    data = request.get_json()
-    user_message = data.get("message", "")
+    config.refresh()
+    if feature_manager.is_enabled("UseNewChatModel"):
+        azure_openai_endpoint = config["AzureOpenAI:Endpoint"]
+        azure_openai_api_key = config["AzureOpenAI:Key"]
+        chat_completions_model_name = config["AzureOpenAI:ModelName"]
 
-    response = client.chat.completions.create(
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": user_message},
-        ],
-        max_tokens=8192,
-        temperature=0.7,
-        top_p=0.95,
-        frequency_penalty=0.0,
-        presence_penalty=0.0,
-        model=AZURE_OPENAI_MODEL_NAME,
-    )
+        # creating the Azure OpenAI Client
+        client = AzureOpenAI(
+            api_version = "2024-06-01",
+            azure_endpoint=azure_openai_endpoint,
+            api_key=azure_openai_api_key
+        )
 
-    return jsonify({
-        "model": response.model,
-        "reply": response.choices[0].message.content
-    })
+        data = request.get_json()
+        user_message = data.get("message", "")
+
+        response = client.chat.completions.create(
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": user_message},
+            ],
+            max_tokens=8192,
+            temperature=0.7,
+            top_p=0.95,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+            model=chat_completions_model_name
+        )
+
+        return jsonify({
+            "model": response.model,
+            "reply": response.choices[0].message.content
+        })
+    else:
+        azure_openai_endpoint = config["AzureOpenAI:Endpoint"]
+        azure_openai_api_key = config["AzureOpenAI:Key"]
+        chat_completions_model_name = config["AzureOpenAI:NewModelName"]
+
+        # creating the Azure OpenAI Client
+        client = AzureOpenAI(
+            api_version = "2024-06-01",
+            azure_endpoint=azure_openai_endpoint,
+            api_key=azure_openai_api_key
+        )
+
+        data = request.get_json()
+        user_message = data.get("message", "")
+
+        response = client.chat.completions.create(
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": user_message},
+            ],
+            max_tokens=8192,
+            temperature=0.7,
+            top_p=0.95,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+            model=chat_completions_model_name
+        )
+
+        return jsonify({
+            "model": response.model,
+            "reply": response.choices[0].message.content
+        })
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
